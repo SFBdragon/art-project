7c46118ce880409a596a9626e5ac2981cc688b1b
SFB <sf.beautement@gmail.com>
2019-07-31 23:54:20 +0200

    initial commit

---
+++ ArtProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArtProject
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameLoop())
+                game.Run();
+        }
+    }
+}
